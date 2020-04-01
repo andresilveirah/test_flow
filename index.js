717a35d91d51e040ae5e3e969361f68b3e34c07b
Andre Herculano <andresilveirah@gmail.com>
2020-04-01 10:09:23 +0200

    grab choice_id from sps api

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
 const https = require('https')
 const { promisify } = require('util');
 
-  const CONFIG = {
-    wrapperApiBase: 'wrapper-api.sp-prod.net',
-    messageUrl: '/tcfv2/v1/gdpr/message-url?inApp=true',
-    nativeUrl: '/tcfv2/v1/gdpr/native-message?inApp=true',
-    consentUrl: '/tcfv2/v1/gdpr/consent?inApp=true',
-  }
+const CONFIG = {
+  messageUrl: new URL('https://wrapper-api.sp-prod.net/tcfv2/v1/gdpr/message-url?inApp=true'),
+  nativeUrl: new URL('https://wrapper-api.sp-prod.net/tcfv2/v1/gdpr/native-message?inApp=true'),
+  consentUrl: new URL('https://wrapper-api.sp-prod.net/tcfv2/v1/gdpr/consent?inApp=true'),
+  messageDataUrl: messageId => new URL(`https://message.sp-prod.net/mms/v2/message?message_id=${messageId}`)
+}
 
-const post = promisify((endpoint, payload, callback) => {
+const responseToJson = callback => response => {
+  var chunks = [];
+  response.on("data", chunk => chunks.push(chunk))
+  response.on("end", _ => callback(null, JSON.parse(Buffer.concat(chunks).toString())))
+}
+
+const get = promisify((url, callback) => {
   const req = https.request({
-    hostname: CONFIG.wrapperApiBase,
-    path: endpoint,
+    hostname: url.hostname,
+    path: url.pathname + url.search,
+    method: 'GET'
+  }, responseToJson(callback))
+  req.on('error', error => callback(error, null))
+  req.end()
+})
+
+const post = promisify((url, payload, callback) => {
+  const req = https.request({
+    hostname: url.hostname,
+    path: url.pathname + url.search,
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Content-Length': payload.length
     }
-  }, response => {
-    var chunks = [];
-    response.on("data", chunk => chunks.push(chunk))
-    response.on("end", _ => callback(null, JSON.parse(Buffer.concat(chunks).toString())))
-  })
+  }, responseToJson(callback))
   req.on('error', error => callback(error, null))
   req.write(payload)
   req.end()
 })
 
+const getNative = async (params) => await post(CONFIG.nativeUrl, JSON.stringify(params))
 const getMessage = async (params) => await post(CONFIG.messageUrl, JSON.stringify(params))
 const postConsent = async (params) => await post(CONFIG.consentUrl, JSON.stringify(params))
 
-module.exports = { getMessage, postConsent }
+const getChoicesForMessageUrl = async (messageUrl) => {
+  const messageId = (new URL(messageUrl)).searchParams.get("message_id")
+  const message = await get(CONFIG.messageDataUrl(messageId))
+  return message.message_choice
+}
+
+const getChoiceIdForAction = choices => action => choices.filter(choice => choice.type === action)[0].choice_id
+
+module.exports = {
+  getMessage,
+  postConsent,
+  getNative,
+  getChoicesForMessageUrl,
+  getChoiceIdForAction
+}
--- index.test.js
+++ index.test.js
@@ -1,34 +1,49 @@
-const { getMessage, postConsent } = require('./index')
+const {
+  getMessage,
+  postConsent,
+  getChoicesForMessageUrl,
+  getChoiceIdForAction
+} = require('./index')
 
-const PARAMS = {
-  accountId: 22,
-  propertyHref: "https://tcfv2.mobile.webview",
-  propertyId: 7639,
-  requestUUID: "test",
-  euconsent: "",
-  meta: "{}",
-  requestFromPM: false,
-  choiceId: "739690",
-  actionType: 11
+jest.setTimeout(10000)
+
+const ACTIONS = {
+  acceptAll: 11
 }
 
 describe('full flow', () => {
-  it('should not return a message url on the 2nd "get" message request', async (done) => {
-    const firstCall = await getMessage(PARAMS)
-    expect(firstCall).toHaveProperty('url')
+  describe('web', () => {
+    let propertyWeb =  {
+      accountId: 22,
+      propertyHref: "https://tcfv2.mobile.webview",
+      propertyId: 7639,
+      requestUUID: "test",
+      euconsent: "",
+      meta: "{}",
+      requestFromPM: false
+    }
+    it('should not return a message url on the 2nd "get" message request', async (done) => {
+      const firstCall = await getMessage(propertyWeb)
+      expect(firstCall).toHaveProperty('url')
 
-    const secondCall = await postConsent({
-      ...PARAMS,
-      meta: firstCall.meta,
-      uuid: firstCall.uuid
+      const choiceId = getChoiceIdForAction(await getChoicesForMessageUrl(firstCall.url))(ACTIONS.acceptAll)
+      expect(choiceId).toBeDefined()
+
+      const secondCall = await postConsent({
+        ...propertyWeb,
+        meta: firstCall.meta,
+        uuid: firstCall.uuid,
+        choiceId: choiceId.toString(),
+        choiceType: ACTIONS.acceptAll
+      })
+      const thirdCall = await getMessage({
+        ...propertyWeb,
+        meta: secondCall.meta,
+        uuid: secondCall.uuid
+      })
+      expect(thirdCall).not.toHaveProperty('url')
+      done()
     })
-    const thirdCall = await getMessage({
-      ...PARAMS,
-      meta: secondCall.meta,
-      uuid: secondCall.uuid
-    })
-    expect(thirdCall).not.toHaveProperty('url')
-    done()
   })
 })
 
